fd8b08c094e0487d41e90dd64c68d7110598d8e4
damarowen <damarowen@gmail.com>
2021-02-01 22:15:35 +0700

    add database

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./database')
 
 const port = 9000;
+
+connectDB();
 
 app.get('/', (req, res)  => {
     res.send('hai damar')
 })
+
 
 app.listen(port, (x) => {
 
